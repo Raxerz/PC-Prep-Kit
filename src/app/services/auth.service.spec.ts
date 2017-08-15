b0fbb66e109309e9e8703dae340b06ac5e6384a6
raxerz <rajath_krishna@yahoo.co.in>
2017-08-15 14:10:10 +0530

    Service test changes

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,13 +1,26 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { fakeAsync, TestBed, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from './auth.service';
-import { HttpModule } from '@angular/http';
+import { HttpModule, XHRBackend, ResponseOptions, Response, RequestMethod } from '@angular/http';
 import { APIService } from './api.service';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { environment } from '../../environments/environment';
+
+const mockLoginResponse = {user: {email: 'abc@gmail.com', name: 'Rajath'}, token: 'abcdef'};
+const loginAuthUrl = environment.baseURL + environment.authEndpoint + 'login';
+const logoutAuthUrl = environment.baseURL + environment.authEndpoint + 'login';
 
 describe('AuthService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [AuthService, APIService],
+            providers: [
+                AuthService, 
+                APIService,
+                {
+                  provide: XHRBackend,
+                  useClass: MockBackend
+                }               
+            ],
 			imports: [
 				RouterTestingModule,
 				HttpModule
@@ -15,7 +28,34 @@ describe('AuthService', () => {
         });
     });
 
-    it('should be created', inject([AuthService], (service: AuthService) => {
-        expect(service).toBeTruthy();
-    }));
+    it('should be created', fakeAsync(
+        inject([
+            XHRBackend, 
+            AuthService
+        ], (mockBackend: MockBackend, service: AuthService) => {
+            mockBackend.connections.subscribe(
+                (connection: MockConnection) => {
+                    if (connection.request.url === logoutAuthUrl) {
+                        expect(connection.request.method).toBe(RequestMethod.Get);
+                        connection.mockRespond(new Response(
+                            new ResponseOptions({})
+                        ));                
+                    } else if (connection.request.url === loginAuthUrl) {
+                        expect(connection.request.method).toBe(RequestMethod.Post);
+                        connection.mockRespond(new Response(
+                            new ResponseOptions({ body: mockLoginResponse })
+                        ));                    
+                    }
+                });            
+                expect(service).toBeTruthy();
+                service.logout()
+                        .subscribe(res => {
+                            expect(service).toBeTruthy();
+                        });                
+                service.loginUser({email: 'abc@gmail.com', password: 'abc'})
+                        .subscribe(res => {
+                            expect(res).toBeTruthy();
+                        });
+            })
+    ));
 });
